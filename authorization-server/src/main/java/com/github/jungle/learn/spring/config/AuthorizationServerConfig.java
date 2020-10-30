af9d07daec4fd7020dfeebe96f1420ee5ee4c409
jungle93 <>
2020-10-30 09:04:25 +0800

    修改工程依赖，添加oauth2依赖

---
+++ authorization-server/src/main/java/com/github/jungle/learn/spring/config/AuthorizationServerConfig.java
@@ -0,0 +1,10 @@
+/**
+ * @description:AuthorizationServerConfig
+ * @author: jungle
+ * @date: 2020/10/30
+ * @version: 1.0
+ */
+package com.github.jungle.learn.spring.config;
+
+public class AuthorizationServerConfig {
+}
